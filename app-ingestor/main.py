27d21f121865c25640fcbe84e1dffabec25c5807
Anthony Caliani <avcaliani.it@gmail.com>
2020-07-17 20:32:25 -0300

    Adding App Ingestor hello world

---
+++ app-ingestor/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from sys import exit
+
+
+def main():
+    print('App Ingestor Works!')
+
+
+if __name__ == "__main__":
+    status = 0
+    try:
+        main()
+    except Exception as ex:
+        print(f'FATAL! {ex}')
+        status = 1
+    exit(status)
