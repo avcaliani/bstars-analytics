4723efd8b64eed2e69b8de466f2ba38764d48afe
Anthony Caliani <avcaliani.it@gmail.com>
2020-07-18 22:35:15 -0300

    Adding parameters and scripts

--- app-collector/main.py
+++ app-collector/main.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
+import logging
 from sys import exit
+
+from utils import arguments, log
+
+APP_NAME, APP_VERSION = 'app-collector', '1.0.0-ALPHA'
 
 
 def main():
+    args = arguments.get()
+    log.init(APP_NAME, APP_VERSION, args.log_path)
+    logging.info(f'Arguments: {args}')
     print('App Collector Works!')
 
 
--- app-collector/run.sh
+++ app-collector/run.sh
@@ -6,12 +6,37 @@
 # @description
 # The following script runs "app-collector".
 #
+# @params
+# 01 - Environment Name
+#
 # @usage
-# ./run.sh
+# ./run.sh local
 PROJECT_DIR="$(dirname $0)"
 
+env=${1:-'local'}
 status=0
-cd ${PROJECT_DIR} \
-  && ./main.py || status=1 \
-  && cd - \
-  && exit "$status"
+cd "$PROJECT_DIR" && source "env/$env.env"
+
+tmp_path=$(mktemp -d)
+tmp_output_path="$tmp_path/data"
+tmp_log_path="$tmp_path/logs"
+
+mkdir -p "$tmp_output_path" "$tmp_log_path"
+
+pip3 install -q -r requirements.txt \
+  && ./main.py --api-url "$API_URL" \
+               --api-token "$API_TOKEN" \
+               --api-retry "$API_RETRY" \
+               --output-path "$tmp_output_path" \
+               --log-path "$tmp_log_path" \
+  || status=1
+
+
+if [[ "$env" == "local" ]]; then
+  mkdir -p "$OUTPUT_PATH" "$LOG_PATH"
+fi
+
+cp -r $tmp_output_path/* $OUTPUT_PATH/
+cp -r $tmp_log_path/* $LOG_PATH/
+
+cd - && exit "$status"
