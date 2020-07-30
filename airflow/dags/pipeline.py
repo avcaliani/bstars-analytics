591cc1254fa28b68d9103d4d52f0fa7bec0fb307
Anthony Caliani <avcaliani.it@gmail.com>
2020-07-30 19:34:04 -0300

    Updating Airflow DAG and Environment

--- airflow/dags/pipeline.py
+++ airflow/dags/pipeline.py
@@ -1,7 +1,13 @@
+from os import environ
 from datetime import datetime, timedelta
 
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
+from airflow.contrib.operators.gcp_compute_operator import GceInstanceStartOperator
+
+PROJECT_ID = environ.get('PROJECT_ID')
+ZONE = environ.get('ZONE')
+APP_COLLECTOR_VM = environ.get('APP_COLLECTOR_VM')
 
 INTERVAL = '@daily'
 YESTERDAY = datetime.now() - timedelta(days=1)
@@ -19,14 +25,20 @@ ARGS = {
 
 with DAG('pipeline', default_args=ARGS, schedule_interval=INTERVAL, catchup=False) as dag:
 
-    first_task = BashOperator(
-        task_id='first_task',
-        bash_command='echo {{ dag_run.id }}'
+    app_collector_start = GceInstanceStartOperator(
+        task_id='app-collector-start',
+        project_id=PROJECT_ID,
+        zone=ZONE,
+        resource_id=APP_COLLECTOR_VM
+    )
+    app_collector_wait = BashOperator(
+        task_id='app-collector-wait',
+        bash_command=(
+            f'gcloud compute instances tail-serial-port-output "{APP_COLLECTOR_VM}" '
+            f'--zone "{ZONE}" '
+            f'--project "{PROJECT_ID}" '
+            f'|| true'
+        )
     )
 
-    second_task = BashOperator(
-        task_id='second_task',
-        bash_command='echo {{ dag_run.id }}'
-    )
-
-    first_task >> second_task
+    app_collector_start >> app_collector_wait
